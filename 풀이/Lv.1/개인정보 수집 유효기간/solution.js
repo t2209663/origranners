7f73ecbf6a070c70b903f86a9e9d49618ab9912d
suin Yeo <immof@naver.com>
2023-09-14 01:31:49 +0900

    feat: 이해하기 쉽게 변환하고 주석 달기

--- 풀이/Lv.1/개인정보 수집 유효기간/solution.js
+++ 풀이/Lv.1/개인정보 수집 유효기간/solution.js
@@ -1,16 +1,19 @@
 function solution(today, terms, privacies) {
-    let result = [];
-    let obj1 = {};
-    for(let e of terms){
-        const [type, month] = e.split(" ");
-        obj1[type] = month;
+    let answer = [];
+    let term = {};
+    for(let terms1 of terms){ //term 객체에 terms type,month로 넣기
+        const [type, month] = terms1.split(" ")
+        term[type] = month
     }
-    for(let i = 0; i < privacies.length; i++){
-        const[start, type] = privacies[i].split(" ");
-        let now = new Date(start);
-        let date = new Date(today);
-        now.setMonth(now.getMonth() + Number(obj1[type]));
-        if(date >= now) result.push(i+1);
+    for(let i=0; i<privacies.length; i++){
+        const [privstart, privtype] = privacies[i].split(" ") //개인정보 수집 일자와 종류 나누기
+        let startDate = new Date(privstart);
+        let nowDate = new Date(today);
+        startDate.setMonth(startDate.getMonth() + Number(term[privtype])); //개인정보 시작 날짜 + 각 개인정보 약관 종류의 유효 기간을 더해서 끝나는 달을 구하기
+        if (nowDate >= startDate) { //nowDate가 크면 유효 기간이 남았으니 answer에 push 하기
+            answer.push(i+1)
+        }
+        
     }
-    return result;
+    return answer;
 }
