4d7652741bcc5e9647cbb865b5228a93d78578cc
yeosuin <immof@naver.com>
2024-03-22 16:03:26 +0900

    :sparkles: feat: 1레벨 성격 유형 검사하기 문제와 해설 추가
    Signed-off-by: yeosuin <immof@naver.com>

---
+++ 풀이/Lv.1/성격 유형 검사하기/solution.js
@@ -0,0 +1,32 @@
+function solution(survey, choices) {
+	let answer = '';
+	const types = { R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0};
+
+	for(let i=0; i<choices.length; i++) {
+		const [leftType, rightType] = survey[i].split("");
+		const result = Math.abs(choices[i] - 4);
+		if (choices[i] < 4) {
+			//왼쪽 타입 비동의
+			types[leftType] += result;
+		}else if (choices[i] > 4) {
+			//오른쪽 타입 동의
+			types[rightType] +=result;
+		}
+	}
+
+	const type = Object.keys(types); //객체 갯수
+
+	for(let i=0; i<type.length; i+=2) {
+		const leftType = types[type[i]];
+		const rightType = types[type[i+1]];
+
+		if(leftType >= rightType) {
+			answer += type[i];
+		}else {
+			answer += type[i+1];
+		}
+	}
+
+
+	return answer;
+}
